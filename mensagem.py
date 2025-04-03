788a4b5c977476ad5bbdc83d7eb0ae0220e837c0
Matheus Dias <diassmatheus@outlook.com>
2025-04-03 20:55:52 -0300

    Inclusão função saudação em mensagem.py

--- mensagem.py
+++ mensagem.py
@@ -3,3 +3,8 @@ def printa_mensagem():
     return 'Hello world, utilizando Git com VSCode e GitHub. (Nova ramificação)'
 
 versao = "nova_ramificacao"
+
+def saudacao(nome):
+    print(f'Seja bem-vindo, {nome}!')
+
+saudacao('Asimov!')
